3c18b4b03eea8bb1b20cf475e728f792dffe3d08
Fabrizio Tappero <fabrizio.tappero@gmail.com>
2015-03-21 19:17:43 +0100

    moved around some stuff and made shop tabs

--- js/generic.js
+++ js/generic.js
@@ -25,16 +25,49 @@ $(function() {
 
 // smoothScroll function is applied from the document ready function
 function smoothScroll (duration) {
-  $('a[href^="#"]').on('click', function(event) {
+  //$('a[href^="#"]').on('click', function(event) {
+  $('a[href*="#"]').on('click', function(event) { // every link tha has "#" in it
 
       var target = $( $(this).attr('href') );
 
-      if( target.length ) {
+      // if link begins with "#" and target exist smoothly go to it
+      if( target.length && target.selector.match('^#') ) {
           event.preventDefault();
+
           // scroll to section
           $('html, body').animate({
               scrollTop: target.offset().top
+              //scrollTop: $(window.location.hash).offset().top
           }, duration);
+      }
+      else{
+        event.preventDefault();
+        var target = $( $(this).attr('href') );
+        console.log(target);
+        //window.location = "/"; // go home
+        window.location = target.selector.replace('home#','/#');
+
       }
   });
 }
+
+// shop item tabs styling
+$(document).ready(function () {
+  $('.accordion-tabs').each(function(index) {
+    $(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
+  });
+
+  $('.accordion-tabs').on('click', 'li > a', function(event) {
+    if (!$(this).hasClass('is-active')) {
+      event.preventDefault();
+      var accordionTabs = $(this).closest('.accordion-tabs');
+      accordionTabs.find('.is-open').removeClass('is-open').hide();
+
+      $(this).next().toggleClass('is-open').toggle();
+      accordionTabs.find('.is-active').removeClass('is-active');
+      $(this).addClass('is-active');
+    } else {
+      event.preventDefault();
+    }
+  });
+});
