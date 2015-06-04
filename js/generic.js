e932d81d0fc54d9585e30eaad84471776a12ca95
Fabrizio <fabriziotappero@users.noreply.github.com>
2015-06-04 14:51:42 +0200

    Update generic.js

--- js/generic.js
+++ js/generic.js
@@ -17,7 +17,12 @@ $("#fittext2").fitText(1.7, { minFontSize: '31px', maxFontSize: '42px' });
  });
 
 // set equale height for all cards in homepage
-$(function(){ $('#equalize').equalHeights(); });
+$(function(){
+ var heig = $("#equalize").height(); // first card encountered
+ $("#equalize").each(function(index) {
+  $(this).height(heig);
+ });
+});
 
 $(function () {
   /* IP Cores search field */
