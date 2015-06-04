d4f23aff3d2ecb3397475b11a0ef4def6be4e23a
Fabrizio Tappero <fabrizio.tappero@gmail.com>
2015-06-04 22:33:09 +0200

    fixed minor margin problems

--- js/generic.js
+++ js/generic.js
@@ -19,7 +19,8 @@ $("#fittext2").fitText(1.7, { minFontSize: '31px', maxFontSize: '42px' });
 $(window).resize(function() {
  var heig = $(".card-inner").first().height(); // first card element encountered
  $("div.card-inner:not(:first)").each(function(index) {
-  //$(this).height(heig);});
+  //$(this).height(heig);
+  });
 });
 
 $(function () {
