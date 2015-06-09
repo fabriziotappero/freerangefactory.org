74d436fee947bdb9ddf88c83429c6a8d30bc7719
Fabrizio <fabriziotappero@users.noreply.github.com>
2015-06-09 14:28:58 +0200

    Update generic.js

--- js/generic.js
+++ js/generic.js
@@ -86,3 +86,20 @@ $(document).ready(function () {
     }
   });
 });
+
+// code for the modal window
+$(function() {
+  $("#modal-1").on("change", function() {
+    if ($(this).is(":checked")) {
+      $("body").addClass("modal-open");
+    } else {
+      $("body").removeClass("modal-open");
+    }
+  });
+  $(".modal-fade-screen, .modal-close").on("click", function() {
+    $(".modal-state:checked").prop("checked", false).change();
+  });
+  $(".modal-inner").on("click", function(e) {
+    e.stopPropagation();
+  });
+});
