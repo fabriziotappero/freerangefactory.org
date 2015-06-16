2202736141570e50f22ec6085c637d2e29757236
Fabrizio <fabriziotappero@users.noreply.github.com>
2015-06-16 15:28:25 +0200

    Update generic.js

--- js/generic.js
+++ js/generic.js
@@ -119,7 +119,7 @@ $(document).ready(function() {
             top: scrollTop < originalY
                     ? 0
                     : scrollTop - originalY + 5
-        }, 300,function(){element.show(3000);});
+        }, 300,function(){element.slideDown(5000,"swing");});
     });
   }
 });
