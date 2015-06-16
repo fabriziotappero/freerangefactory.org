3d066128c32471f36aaf1a00fdbe54418a984b88
Fabrizio <fabriziotappero@users.noreply.github.com>
2015-06-16 10:25:52 +0200

    Update generic.js

--- js/generic.js
+++ js/generic.js
@@ -116,7 +116,7 @@ $(window).scroll(function() {
   //$(".links").css("top", $(window).scrollTop() + "px");
  $(".links").animate({
     "top": $(window).scrollTop() + "px"
-  }, 10, function() {
+  }, 300, function() {
   });
 
   justExecuted = true;
